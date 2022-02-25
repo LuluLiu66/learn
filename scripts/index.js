f12ee755d301b3b11c493cbe39281a6c763f92d0
teresa.l <teresa.l@binance.com>
2022-02-25 10:25:00 +0800

    send a contract

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+module.exports = async function main (callback) {
+    try {
+      // Our code will go here
+      // Retrieve accounts from the local node
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      callback(0);
+    } catch (error) {
+      console.error(error);
+      callback(1);
+    }
+  };
